7d4b70571a2b2a0cdc89120da687717d336db0f9
Julian Power <josejulianpower@gmail.com>
2023-04-20 22:07:01 -0600

    adding more tests

--- backend/db/models/jobs.py
+++ backend/db/models/jobs.py
@@ -1,19 +1,31 @@
-from backend.db.database import Base 
-from sqlalchemy import Column,Integer,Date,String,Boolean,TIMESTAMP,ForeignKey,ForeignKeyConstraint
+from ...db.database import Base
+from sqlalchemy import (
+    Column,
+    Integer,
+    Date,
+    String,
+    Boolean,
+    TIMESTAMP,
+    ForeignKey,
+    ForeignKeyConstraint,
+)
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 
+
 class Jobs(Base):
-    
-    __tablename__="jobs"
-    
-    id = Column(Integer,primary_key=True,index=True)
-    title = Column(String,nullable=False)
-    company = Column(String,nullable=False)
+
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    company = Column(String, nullable=False)
     company_url = Column(String)
-    location = Column(String,nullable=False)
-    description = Column(String,nullable=False)
-    date_posted = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text("NOW()"))
-    is_active = Column(Boolean(),default=True)
-    owner_id = Column(Integer,ForeignKey("user.id"))
-    owner = relationship("User",back_populates='jobs')
+    location = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    date_posted = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()")
+    )
+    is_active = Column(Boolean(), default=True)
+    owner_id = Column(Integer, ForeignKey("user.id"))
+    owner = relationship("User", back_populates="jobs")
--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -1,33 +1,41 @@
-from fastapi.testclient import TestClient 
-from backend.schemas.Jobs import job_show 
+from fastapi.testclient import TestClient
+from backend.schemas.Jobs import job_show
 from typing import List
-def test_create_job(authorized_client:TestClient,test_user):
-    data={
-        "title":"Job1",
-        "company":"company1",
-        "company_url":"www.doodle.com",
-        "location":"localhost",
-        "description":"python"
+
+
+def test_create_job(authorized_client: TestClient, test_user):
+    data = {
+        "title": "Job1",
+        "company": "company1",
+        "company_url": "www.doodle.com",
+        "location": "localhost",
+        "description": "python",
     }
-    
-    res= authorized_client.post("/jobs/create",json=data)
-    
+
+    res = authorized_client.post("/jobs/create", json=data)
+
     assert res.status_code == 200
     assert res.json().get("title") == data["title"]
-    
-def test_get_jobs(client:TestClient,create_jobs:List[job_show]):
-    
+
+
+def test_get_jobs(client: TestClient, create_jobs: List[job_show]):
+
     res = client.get("/jobs/")
-    
+
     def validate_jobs(job):
-        return job_show(**job) 
-        
-    
+        return job_show(**job)
+
     if len(res.json()) > 1:
-        
-        job_map = map(validate_jobs,res.json())
+
+        job_map = map(validate_jobs, res.json())
         jobs = list(job_map)
-        assert res.status_code == 200 
+        assert res.status_code == 200
         assert jobs[0].title == create_jobs[0].title
         assert len(jobs) == len(create_jobs)
-        
+
+
+def test_get_one_job(client: TestClient, create_jobs: List[job_show]):
+    res = client.get("/jobs/?=1")
+
+    assert res.status_code == 200
+    assert res.json()[0]["title"] == create_jobs[0].title
